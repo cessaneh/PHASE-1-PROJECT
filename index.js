550407d112ad96e381bd3a04b2ba38e1319386d4
princessmumbi <princessmumbi001@gmail.com>
2024-04-10 11:16:23 +0300

    phase-1-project

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener ("DOMContentLoaded", function() {
     const dogImgsContainer = document.getElementById("dog-images")
     const dogbtn = document.getElementById("new-dogbtn");
-    const likebtns = document.getElementById("like-btn");
-    const commentbtns = document.getElementById("comment-btn");
+    // const likebtns = document.getElementById("like-btn");
+    // const commentbtns = document.getElementById("comment-btn");
     function addDogImages () {
         fetch ("https://random.dog/woof.json")
         .then(res => res.json())
@@ -19,8 +19,8 @@ document.addEventListener ("DOMContentLoaded", function() {
     dogbtn.addEventListener ("mouseenter",{
         handleEvent: (e) => {e.target.style.backgroundColor ="pink"}
     })
-    likebtns.addEventListener("click",addlike)
-    commentbtns.addEventListener("click",addComment)
+    // likebtns.addEventListener("click",addlike)
+    // commentbtns.addEventListener("click",addComment)
     addDogImages()
 
 }) 
